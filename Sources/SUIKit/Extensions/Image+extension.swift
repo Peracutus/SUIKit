df875c32704e9b706718a9ebcb1896a57f7dbf12
Peracutus <pink>
2025-03-27 03:00:33 -0700

    enums

--- Sources/SUIKit/Extensions/Image+extension.swift
+++ Sources/SUIKit/Extensions/Image+extension.swift
@@ -8,11 +8,73 @@
 import SwiftUI
 
 public enum SettingsImage: String {
-    case globe, currency, cats, noti, key, terms, faceid, info, feedback
+    case globe, currency, category, noti, key, terms, faceid, info, feedback
     
     public var getImage: Image {
         switch self {
-        case .globe, .currency, .cats, .noti, .key, .terms, .faceid, .info, .feedback:
+        case .globe, .currency, .category, .noti, .key, .terms, .faceid, .info, .feedback:
+            return Image(rawValue, bundle: .module)
+        }
+    }
+}
+
+public enum CategoryImage: String, CaseIterable {
+    case Airplane
+    case Babys
+    case Banking
+    case Barbershop
+    case Beach
+    case Beer
+    case Bicycle
+    case CarRental
+    case Cars
+    case Children
+    case Clinic
+    case Clothes
+    case Cocktail
+    case CoffeeToGo
+    case Controller
+    case CookingPot
+    case CreditCard
+    case Documentary
+    case Exterior
+    case FoodOut
+    case GasStation
+    case Gift
+    case Grapes
+    case Grocery
+    case Hanger
+    case Hearts
+    case Hotels
+    case Iphone
+    case Jewelry
+    case Jym
+    case Kitchenwares
+    case Laptop
+    case Literature
+    case LivingRoom
+    case Mastercard
+    case MoneyTransfer
+    case Music
+    case Puzzle
+    case Restaurant
+    case Sale
+    case ShoppingBag
+    case ShoppingCartLoaded
+    case SimCard
+    case SmartphoneTablet
+    case Taxi
+    case TheatreMask
+    case Ticket
+    case Tomato
+    case Transport
+    case Travel
+    case Truck
+    case Visa
+    
+    public var getImage: Image {
+        switch self {
+        case.Airplane,.Babys,.Banking,.Barbershop,.Beach,.Beer,.Bicycle,.CarRental,.Cars,.Children,.Clinic,.Clothes,.Cocktail,.CoffeeToGo,.Controller,.CookingPot,.CreditCard,.Documentary,.Exterior,.FoodOut,.GasStation,.Gift,.Grapes,.Grocery,.Hanger,.Hearts,.Hotels,.Iphone,.Jewelry,.Jym,.Kitchenwares,.Laptop,.Literature,.LivingRoom,.Mastercard,.MoneyTransfer,.Music,.Puzzle,.Restaurant,.Sale,.ShoppingBag,.ShoppingCartLoaded,.SimCard,.SmartphoneTablet,.Taxi,.TheatreMask,.Ticket,.Tomato,.Transport,.Travel,.Truck,.Visa:
             return Image(rawValue, bundle: .module)
         }
     }
--- Sources/SUIKit/Extensions/String+extension.swift
+++ Sources/SUIKit/Extensions/String+extension.swift
@@ -7,61 +7,6 @@
 
 public extension String {
     
-    enum CategoryImage: String, CaseIterable {
-        case Airplane
-        case Babys
-        case Banking
-        case Barbershop
-        case Beach
-        case Beer
-        case Bicycle
-        case CarRental
-        case Cars
-        case Children
-        case Clinic
-        case Clothes
-        case Cocktail
-        case CoffeeToGo
-        case Controller
-        case CookingPot
-        case CreditCard
-        case Documentary
-        case Exterior
-        case FoodOut
-        case GasStation
-        case Gift
-        case Grapes
-        case Grocery
-        case Hanger
-        case Hearts
-        case Hotels
-        case Iphone
-        case Jewelry
-        case Jym
-        case Kitchenwares
-        case Laptop
-        case Literature
-        case LivingRoom
-        case Mastercard
-        case MoneyTransfer
-        case Music
-        case Puzzle
-        case Restaurant
-        case Sale
-        case ShoppingBag
-        case ShoppingCartLoaded
-        case SimCard
-        case SmartphoneTablet
-        case Taxi
-        case TheatreMask
-        case Ticket
-        case Tomato
-        case Transport
-        case Travel
-        case Truck
-        case Visa
-    }
-    
     /// CategoryColors
     enum CategoryColors: String {
         case CatBlue = "#0432FF"
