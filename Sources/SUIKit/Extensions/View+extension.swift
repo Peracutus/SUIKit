defe20aae1f2bfeefb4c2897e129d1b77c213caa
Peracutus <peracutus@MacBook-Pro-Roman.local>
2025-09-17 21:01:46 +0700

    new items

--- Sources/SUIKit/Extensions/View+extension.swift
+++ Sources/SUIKit/Extensions/View+extension.swift
@@ -6,6 +6,26 @@
 //
 
 import SwiftUI
+import Foundation
+
+public struct TrayConfig {
+    var maxDetent: PresentationDetent
+    var cornerRadius: CGFloat
+    var isInteractiveDismissDisabled: Bool
+    var contentInsets: EdgeInsets
+    
+    public init(
+        maxDetent: PresentationDetent = .fraction(0.99),
+        cornerRadius: CGFloat = 16,
+        isInteractiveDismissDisabled: Bool = false,
+        contentInsets: EdgeInsets = .init(top: 0, leading: 0, bottom: 0, trailing: 0)
+    ) {
+        self.maxDetent = maxDetent
+        self.cornerRadius = cornerRadius
+        self.isInteractiveDismissDisabled = isInteractiveDismissDisabled
+        self.contentInsets = contentInsets
+    }
+}
 
 public extension View {
     
@@ -27,6 +47,59 @@ public extension View {
                         })
                 })
             }
+    }
+    
+    @available(iOS 16.4, *)
+    @ViewBuilder
+    func systemTrayView<Content: View>(
+        _ show: Binding<Bool>,
+        config: TrayConfig = .init(),
+        @ViewBuilder content: @escaping () -> Content
+    ) -> some View {
+        self
+            .sheet(isPresented: show) {
+                VStack(spacing: 0) {
+                    content()
+                        .padding(config.contentInsets)
+                }
+                .background(
+                    RoundedRectangle(cornerRadius: config.cornerRadius, style: .continuous)
+                        .fill(Color(.systemBackground))
+                        .ignoresSafeArea(edges: .bottom)
+                )
+                .presentationDetents([config.maxDetent])
+                .presentationCornerRadius(config.cornerRadius)
+                .presentationBackground(.clear)
+                .presentationDragIndicator(.hidden)
+                .interactiveDismissDisabled(config.isInteractiveDismissDisabled)
+            }
+    }
+}
+
+fileprivate extension UIView {
+    var dropShadowView: UIView? {
+        if let superview, String(describing: type(of: superview)) == "UIDropShadowView" {
+            return superview
+        }
+        return superview?.dropShadowView
+    }
+}
+
+fileprivate struct RemoveSheetShadow: UIViewRepresentable {
+    func makeUIView(context: Context) -> some UIView {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .clear
+        
+        DispatchQueue.main.async {
+            if let shadowView = view.dropShadowView {
+                shadowView.layer.shadowColor = Color.clear.cgColor
+            }
+        }
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+         
     }
 }
 
---
+++ Sources/SUIKit/Helpers/TrayHelper.swift
@@ -0,0 +1,10 @@
+//
+//  TrayHelper.swift
+//  SUIKit
+//
+//  Created by Роман Рунов on 01.04.2025.
+//
+
+import SwiftUI
+
+
